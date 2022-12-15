c0343b02a02adc37e4555566694b35264603c400
Mariah Laqua <mariahlaqua@gmail.com>
2022-12-15 11:32:44 +0100

    Updated test for clarity, changed hardcoded values to variables

--- tests/testValidPassword.js
+++ tests/testValidPassword.js
@@ -1,19 +1,22 @@
 /***********
  * Test password validation method
- * 
+ * Enter email and password to test in userEmail and password variables as strings
  **************/
 
 const { Sequelize } = require('sequelize');
 const User  = require('../models/User')
 const bcrypt = require('bcrypt');
 const seq = require('../config/database')
+let userEmail = "";
+userEmail = userEmail.toLowerCase();
+let password = "";
 
 seq;
 let user
 async function testPassword(){
     try{
-    user = await User.findOne({where: {email: 'hi@email.com'}})
-    let result = await user.validPassword('papaya')
+    user = await User.findOne({where: {email: userEmail}})
+    let result = await user.validPassword(password)
     console.log(result)
     }catch(error){
         console.log(error)
